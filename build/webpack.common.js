025914b2a8850a02ec10dbe3c8e08750a028450e
qunzi0214 <qunzi0214@gmail.com>
2020-12-01 19:31:25 +0800

    build(webpack babel): add and config webpack babel.etc

---
+++ build/webpack.common.js
@@ -0,0 +1,91 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const LessPluginAutoPrefix = require('less-plugin-autoprefix')
+
+module.exports = {
+  entry: {
+    app: path.resolve(__dirname, '../src/index.tsx'),
+  },
+  output: {
+    filename: '[name].[chunkhash:8].js',
+    path: path.resolve(__dirname, '../dist'),
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '../src'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.tsx?$/,
+        use: [
+          'babel-loader',
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true,
+            },
+          },
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2,
+              modules: {
+                localIdentName: '[local]--[hash:base64:5]',
+              },
+            },
+          },
+          {
+            loader: 'less-loader',
+            options: {
+              lessOptions: {
+                plugins: [
+                  new LessPluginAutoPrefix({
+                    enable: true,
+                    options: {
+                      browsers: ['last 3 versions'],
+                    },
+                  }),
+                ],
+              },
+              sourceMap: false,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          name: '[name].[hash:8].[ext]',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: path.resolve(__dirname, '../public/index.html'),
+      inject: true,
+    }),
+    new CleanWebpackPlugin(),
+  ],
+}
